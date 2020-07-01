49b8dd39478ef366c80bdc04e528e8f19e7983ec
Matias Gastellu Marquez <61754803+MGastelluM@users.noreply.github.com>
2020-07-01 06:43:59 -0400

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,36 +1,28 @@
-
 // Matías Gastellú Marquez
 // fibonacci (O(N))
 import java.util.HashMap;
 
 class fibonacci {
-
+    
     public static int fib(int n, HashMap<Integer, Integer> list) {
-
         if (n <= 1) {
             return n;
         } else {
-
             if ( list.get(n - 1) != null &&  list.get(n - 2) != null) {
                 int a = (Integer) list.get(n - 1);
                 int b = (Integer) list.get(n - 2);
                 return a + b;
             }
-
             list.put(n - 1, fib(n - 1, list));
             list.put(n - 2, fib(n - 2, list));
             return fib(n, list);
-
         }
     }
-
+    
     public static void main(String[] args) {
-
         int n = 45; // Solution works with 0 <= n <= 45 accurately, after that Java cant handle
                       // accurately "int type" results higher than Integer.MAX_VALUE
         HashMap<Integer, Integer> results = new HashMap<>(n+1,1);
-
         System.out.println(fib(n, results));
-
     }
 }
